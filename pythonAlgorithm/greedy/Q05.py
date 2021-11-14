cd37450e9fa45958fb8bd27dcfb9e8bcafb72971
joannekim0420 <sall1412@naver.com>
2021-11-14 22:35:41 +0900

    11-14

--- pythonAlgorithm/greedy/Q05.py
+++ pythonAlgorithm/greedy/Q05.py
@@ -1,14 +1,14 @@
-n, m = map(int, input().split())
-ball = list(map(int, input().split()))
-
-count, i = 0, 0
-for x in ball:
-    i+=1
-    for y in ball[i:n+1]:
-        if x != y:
-          count +=1
-
-print(count)
+# n, m = map(int, input().split())
+# ball = list(map(int, input().split()))
+#
+# count, i = 0, 0
+# for x in ball:
+#     i+=1
+#     for y in ball[i:n+1]:
+#         if x != y:
+#           count +=1
+#
+# print(count)
 
 # array = [0]*11
 #
@@ -19,6 +19,49 @@ print(count)
 #     n -= array[i]
 #     result += array[i] *n
 # print(result)
+
+
+m,n = map(int,input().split())
+BBalls = list(map(int, input().split()))
+
+count211114 = 0
+for i in range(m):
+    n = i+1
+    for j in range(n-1):
+        if BBalls[i] != BBalls[j]:
+            count211114 += 1
+        else:
+            pass
+
+print(count211114)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
