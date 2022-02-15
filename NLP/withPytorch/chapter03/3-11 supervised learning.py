e733fc9651f110025fa162ba4d488367a58b1a07
joannekim0420 <sall1412@naver.com>
2022-02-15 16:16:10 +0900

    02-15 chapter3

---
+++ NLP/withPytorch/chapter03/3-11 supervised learning.py
@@ -0,0 +1,25 @@
+n_epochs = 10
+n_batches = 16
+batch_size = 16
+
+for epoch_i in range(n_epochs):
+    for batch_i in range(n_batches):
+        #step0 prepare data
+        x_data, y_data = get_toy_data(batch_size)
+
+        #step1 initialize gradient
+        perceptron.zero_grad()
+
+        #step2 forward propagation
+        y_pred = perceptron(x_data, apply_sigmoid= True)
+
+        #step3 calculcate loss
+        loss = bce_loss(y_pred, y_target)
+
+        #step4 back propagation
+        loss.backward()
+
+        #step5 update parameter with optimizer
+        optimizer.step()
+
+        
